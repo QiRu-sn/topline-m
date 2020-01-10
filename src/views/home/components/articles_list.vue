e38abf9d1fadb2faecd67ba696ce6d53ed83676c
QiRu <916331669@qq.com>
2020-01-10 15:16:25 +0800

    首页——文章列表组件

---
+++ src/views/home/components/articles_list.vue
@@ -0,0 +1,18 @@
+<template>
+<div>{{channel.name}}的文章列表</div>
+</template>
+
+<script>
+export default {
+  props: ['channel'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,16 +5,21 @@
     <!-- /导航标题栏 -->
     <!-- 频道列表 -->
     <van-tabs v-model="active">
-      <van-tab :title="item.name" v-for="item in channels" :key="item.id"></van-tab>
+      <van-tab :title="item.name" v-for="item in channels" :key="item.id">
+        <articleList :channel='item'/>
+      </van-tab>
     </van-tabs>
     <!-- /频道列表 -->
-
   </div>
 </template>
 
 <script>
 import { getChannels } from '@/api/user'
+import articleList from './components/articles_list'
 export default {
+  components: {
+    articleList
+  },
   data () {
     return {
       active: 0,
@@ -22,6 +27,7 @@ export default {
     }
   },
   methods: {
+    // 获取用户频道列表
     async getUserChannels () {
       let res = await getChannels()
       this.channels = res.data.data.channels
