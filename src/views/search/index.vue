6437a50ab130eade5074fe629b4380a6ecd3d984
QiRu <916331669@qq.com>
2020-01-12 19:59:07 +0800

    文章搜索——搜索关键字高亮

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -8,17 +8,23 @@
       shape="round"
       @input="OnsearchInput"
       @search="onSearch"
+      @cancel="onCancel"
     >
-      <div slot="action">搜索</div>
+      <div slot="action" @click="$router.back()">取消</div>
     </van-search>
     <!-- /搜索框 -->
+    <!-- 搜索结果 -->
+    <ArticleList v-if="isResultsShow"/>
+    <!-- /搜索结果 -->
     <!-- 联想建议 -->
-    <van-cell-group>
-      <van-cell v-for="(item,index) in suggestionList" :key="index" :title="item" icon="search" />
+    <van-cell-group v-else-if="searchText">
+      <van-cell v-for="(item,index) in suggestionList" :key="index"  icon="search" >
+        <div slot="title" v-html="highLight(item)"></div>
+      </van-cell>
     </van-cell-group>
     <!-- /联想建议 -->
     <!-- 搜索记录 -->
-    <van-cell-group>
+    <van-cell-group v-else>
       <van-cell title="历史记录">
         <span>全部删除</span>&nbsp;&nbsp;&nbsp;
         <span>完成</span>&nbsp;&nbsp;&nbsp;
@@ -34,26 +40,38 @@
 
 <script>
 import { getSuggestions } from '@/api/search'
+import ArticleList from './component/articleList'
 export default {
+  components: {
+    ArticleList
+  },
   data () {
     return {
       searchText: '',
       suggestionList: [],
-      historyList: []
+      historyList: [],
+      isResultsShow: false
     }
   },
   methods: {
     async onSearch () {
+      console.log('onsearch')
+    },
+    onCancel () {
+      console.log('onCancel')
     },
     // 获取联想建议内容
     async OnsearchInput () {
       const suggestionText = this.searchText.trim()
       if (!suggestionText) {
-        this.suggestionList = []
-      } else {
-        const { data } = await getSuggestions({ q: suggestionText })
-        this.suggestionList = data.data.options
+        return
       }
+      const { data } = await getSuggestions({ q: suggestionText })
+      this.suggestionList = data.data.options
+    },
+    // 搜索关键字高亮
+    highLight (str) {
+      return str.toLowerCase().replace(this.searchText.toLowerCase(), `<span style="color:red">${this.searchText}</span>`)
     }
   },
   created () {}
