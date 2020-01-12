b1ddd54e84dc79082ca4ecf907e0cb4ef28a353c
QiRu <916331669@qq.com>
2020-01-12 09:45:19 +0800

    文章搜索——页面布局搭建

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>搜索</div>
+  <div>
+    <!-- 搜索框 -->
+    <van-search
+      v-model="searchText"
+      placeholder="请输入搜索关键词"
+      show-action
+      shape="round"
+      @search="onSearch"
+    >
+      <div slot="action" @click="onSearch">搜索</div>
+    </van-search>
+    <!-- /搜索框 -->
+    <!-- 联想建议 -->
+    <van-cell-group>
+      <van-cell title="1" icon="search" />
+      <van-cell title="2" icon="search" />
+      <van-cell title="3" icon="search" />
+      <van-cell title="4" icon="search" />
+      <van-cell title="5" icon="search" />
+    </van-cell-group>
+    <!-- /联想建议 -->
+    <!-- 搜索记录 -->
+    <van-cell-group>
+      <van-cell title="历史记录">
+        <span>全部删除</span>&nbsp;&nbsp;&nbsp;
+        <span>完成</span>&nbsp;&nbsp;&nbsp;
+        <van-icon name="delete"/>
+      </van-cell>
+      <van-cell title="hello"><van-icon name="close" /></van-cell>
+      <van-cell title="hello"><van-icon name="close" /></van-cell>
+      <van-cell title="hello"><van-icon name="close" /></van-cell>
+      <van-cell title="hello"><van-icon name="close" /></van-cell>
+    </van-cell-group>
+    <!-- /搜索记录 -->
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang='less' scoped>
 </style>
